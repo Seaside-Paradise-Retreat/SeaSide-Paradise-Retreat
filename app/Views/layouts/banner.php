d20a3fa6995766b65df3e82d60e09b6d5c34ed85
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2023-12-18 23:03:45 +0700

    build: create home.js

---
+++ app/Views/layouts/banner.php
@@ -0,0 +1,36 @@
+<div class="content-area">
+    <div class="background-images"></div>
+    <div class="content-text">
+      <h1>SEA HOTEL</h1>
+      <h2>50 Floors</h2>
+      <h3>Mỹ Khê - Đà Nẵng</h3>
+      <div class="content_star">
+        <hr>  
+        <div class="star-icons">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <hr>
+      </div>
+      <p class="tel">TEL 0987654321</p>
+      <hr class="long-hr">
+    </div>
+</div>
+<div class="content-aboutus">
+<div class="row aboutus_introduce">
+        <div class="picture">
+            <div class="paragrap_introduce">
+                <h3>About us </h3>
+                <h5>Introduce</h5>
+                <p > Seahotel is an exquisite beach resort that offers a luxurious and unforgettable vacation experience. With stunning ocean views, modern architecture, and dedicated staff, guests can expect top-notch service throughout their stay. Indulge in diverse dining options, relax by the beachfront pool, and engage in thrilling water sports activities. Seahotel is committed to providing a memorable and exceptional beach retreat.</p>
+            </div>
+            <img class="img1 detail_img" id="img1" src="../../Assets/images/room_hotel_aboutus3.jpg" alt="Hình ảnh phòng1">
+            <img class="img2 detail_img" id="img2" src="../../Assets/images/room_hotel_aboutus4.jpg" alt="Hình ảnh phòng 2">
+            <img class="img3 detail_img" id="img3" src="../../Assets/images/room_hotel_aboutus2.jpg" alt="Hình ảnh phòng 3">
+            <img class="img4 detail_img" id="img4" src="../../Assets/images/room_hotel_aboutus1.jpg" alt="Hình ảnh phòng 4">                      
+        </div>
+        </div>
+</div>

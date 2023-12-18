79faa55f3a42444a0a344ba961254cabf9985f0c
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2023-12-18 21:20:42 +0700

    docs/detail_room

---
+++ app/Views/room/show.php
@@ -0,0 +1,142 @@
+<link rel="stylesheet" href="../../../public/css/show.css">
+    <div class="row details_room">
+        <div class="picture">
+            <img class="img1 detail_img" id="img1" src="../../Assets/images/room01.jpg" alt="Hình ảnh phòng 1">
+            <img class="img2 detail_img" id="img2" src="../../Assets/images/roomDetail1.1.jpg" alt="Hình ảnh phòng 2">
+            <img class="img3 detail_img" id="img3" src="../../Assets/images/roomDetail1.2.jpg" alt="Hình ảnh phòng 3">
+            <img class="img4 detail_img" id="img4" src="../../Assets/images/roomDetail1.3.jpg" alt="Hình ảnh phòng 4">
+            <img class="img5 detail_img" id="img5" src="../../Assets/images/roomDetail1.4.jpg" alt="Hình ảnh phòng 5">                         
+        </div>
+
+        <div class="row content">
+            <div class="col-lg-9 col-12">
+            <!-- Show image -->
+                <div class="row content-text">
+                    <div class="col-lg-4 col-6 content__text">
+                        <p id="name_room_detail">Room 101</p>
+                        <p id="price_room_detail">$50/night</p>
+                    </div>
+                    <div class="col-lg-4 col-6 context__icon">
+                        <i id="icon_heart_detail" class="fa-regular fa-heart"></i>
+                        <i id="icon_share_detail" class="fa-solid fa-share"></i>
+                        <div id="addbag"></div>
+                    </div>
+                </div>
+            <!-- Convenient -->
+                <div class="row content-icon">
+                    <div class="col-lg-3 my-2 mx-2 pt-2 icon__room">
+                        <i class="fa-solid fa-bed"></i>
+                        <p id="type1">1 Bedroom</p>
+                    </div>
+
+                    <div class="col-lg-3 my-2 mx-2 pt-2 icon__room">
+                        <i class="fa fa-bath" aria-hidden="true"></i>
+                        <p id="type2">1 Bathroom</p>
+                    </div>
+
+                    <div class="col-lg-3 my-2 mx-2 pt-2 icon__room">
+                        <img src="../../Assets/images/icon_lvroom.png" alt="" style="width: 34px;height: 25px;">
+                        <p id="type3">1 Livingroom</p>
+                    </div>
+                </div>
+
+                <div class="row content-description">
+                    <div class="col-lg-9">
+                        <h4>Room Description</h4>
+                        <p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatu</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    </div>
+                </div>
+            <!-- tổng trung bình sao -->
+                <div class="row content-review">
+                    <div class="col-lg-2 col-6 ">
+                        <h4>Reviews</h4>
+                        <p>Convenient</p>
+                        <p>Clean</p>
+                        <p>Staff</p>
+                        <p>Quality room</p>
+                    </div>
+                    <div class="col-lg-1 col-6 text-end">
+                        <div class="d-flex justify-content-end">
+                            <i class="fa-solid fa-star  pe-2 " style="color: #3A8CED"></i>
+                            <h4>5.0</h4>  
+                        </div>
+                        <p id="convenient">5.0</p>
+                        <p id="clean">5.0</p>
+                        <p id="staff">5.0</p>
+                        <p id="quality">5.0</p>
+                    </div>    
+                </div>
+            <!-- comment detail -->
+            <div class="row content_comment-detail">
+                <div class="col-lg-5 comment-cus">
+                    <div class="row">
+                        <div class="col-lg-2 col-3 ">
+                            <img id="avata1" class="avata__review" src="../../Assets/images/person01.jpg" alt="">  
+                        </div>
+                        <div class="col-lg-4 col-6">
+                            <h5>jondoberman</h5>
+                            <p>Mar 12 2022</p>
+                        </div>
+                    </div>
+                    <div class="row ">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis est eaque inventore laborum deleniti doloribus, quasi quae ducimus deserunt officiis maiores laboriosam quaerat, voluptates autem enim, culpa vitae rerum. Facilis!</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 comment-cus">
+                    <div class="row">
+                        <div class="col-lg-2 col-3">
+                            <img id="avata1" class="avata__review" src="../../Assets/images/person02.jpg" alt="">  
+                        </div>
+                        <div class="col-lg-4 col-6">
+                            <h5>jondoberman</h5>
+                            <p>Mar 12 2022</p>
+                        </div>
+                    </div>
+                    <div class="row ">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis est eaque inventore laborum deleniti doloribus, quasi quae ducimus deserunt officiis maiores laboriosam quaerat, voluptates autem enim, culpa vitae rerum. Facilis!</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 comment-cus">
+                    <div class="row">
+                        <div class="col-lg-2 col-3">
+                            <img id="avata1" class="avata__review" src="../../Assets/images/person03.jpg" alt="">  
+                        </div>
+                        <div class="col-lg-4 col-6">
+                            <h5>jondoberman</h5>
+                            <p>Mar 12 2022</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis est eaque inventore laborum deleniti doloribus, quasi quae ducimus deserunt officiis maiores laboriosam quaerat, voluptates autem enim, culpa vitae rerum. Facilis!</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 comment-cus">
+                    <div class="row">
+                        <div class="col-lg-2 col-3 ">
+                            <img id="avata1" class="avata__review" src="../../Assets/images/person04.jpg" alt="">  
+                        </div>
+                        <div class="col-lg-4 col-6">
+                            <h5>jondoberman</h5>
+                            <p>Mar 12 2022</p>
+                        </div>
+                    </div>
+                    <div class="row ">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis est eaque inventore laborum deleniti doloribus, quasi quae ducimus deserunt officiis maiores laboriosam quaerat, voluptates autem enim, culpa vitae rerum. Facilis!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+          <!-- Nút booking -->
+        <div class="col-lg-3 col-12" id="booking-btn">
+            <a><button id="booking_now" class="button_booking">BOOKING NOW</button></a>
+        </div>
+    </div> 
+      </div>
+    </div>
+  </div>
+</div>

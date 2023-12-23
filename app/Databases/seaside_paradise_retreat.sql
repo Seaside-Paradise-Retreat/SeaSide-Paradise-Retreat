4cc61278be420200a873b711a376203cdccbf42f
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2023-12-23 13:27:04 +0700

    fix: add field rule into table user

--- app/Databases/seaside_paradise_retreat.sql
+++ app/Databases/seaside_paradise_retreat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2023 at 06:33 AM
+-- Generation Time: Dec 23, 2023 at 06:46 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 

658095508c6e1f39e6bd18c1f846c227e0881e8d
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2023-12-29 15:58:41 +0700

    fix: display of room names

--- app/Controllers/detail_room/detail_room.controller.php
+++ app/Controllers/detail_room/detail_room.controller.php
@@ -1,11 +1,11 @@
 <?php
-    require_once ("app/Models/home/card.model.php");
+    require_once ("app/Models/detail/detail.model.php");
     require_once ("app/Models/home/detailroom.model.php");
     if (isset($_GET['id_room'])) {
         $roomId = $_GET['id_room'];
         $images = getRoomImages($roomId);
-        $rooms = getRooms();
-        $room = $rooms[$roomId];
+        $rooms = getRoomId($roomId);
+        // $room = $rooms[$roomId];
     }
 ?>
 <?php 

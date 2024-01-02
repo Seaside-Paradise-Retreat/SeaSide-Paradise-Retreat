94c56afb43d4abf550f838d011d92750beb4f826
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-01-02 19:53:57 +0700

    feat/booking_history

--- app/Views/home/card.php
+++ app/Views/home/card.php
@@ -14,28 +14,9 @@
                     <a href="/detail_room?id_room=<?php echo $room['id']; ?>">
                       <img id="img-card-room" src="<?php echo $image['image_url']?>" height="238px" alt="">
                     </a>
-                    <div class="heart-container position-absolute bottom-0 end-0 m-3" title="Like">
-                      <input type="checkbox" class="checkbox" id="Give-It-An-Id">
-                      <div class="svg-container">
-                          <svg viewBox="0 0 24 24" class="svg-outline" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z">
-                              </path>
-                          </svg>
-                          <svg viewBox="0 0 24 24" class="svg-filled" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z">
-                              </path>
-                          </svg>
-                          <svg class="svg-celebrate" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-                              <polygon points="10,10 20,20"></polygon>
-                              <polygon points="10,50 20,50"></polygon>
-                              <polygon points="20,80 30,70"></polygon>
-                              <polygon points="90,10 80,20"></polygon>
-                              <polygon points="90,50 80,50"></polygon>
-                              <polygon points="80,80 70,70"></polygon>
-                          </svg>
-                      </div>
+                    <div class="heart-container position-absolute bottom-10 end-0 m-3" title="Like">
+                      
                     </div>
-                    <!-- <i class="fas fa-heart position-absolute bottom-0 end-0 m-3" style="font-size: 24px;color:white;"></i> -->
                   </div>
                   <?php $active = false; ?>
                 <?php endforeach; ?>
@@ -51,8 +32,9 @@
             </div>
           </div>
           <div class="mt-2">
-                  <div class="d-flex justify-content-between mb-1">
+                  <div class="d-flex justify-content-between  mb-1">
                     <h5 class="mb-0"><?php echo $room['name']?></h5>
+                    <a href="/?id_room=<?php echo $room['id']; ?>"><i class="fas fa-heart" id="like_favourite" ></i></a>
                     <div id="ratingContainer">
                     <i class="fa-solid fa-star" style="color: #3A8CED;"></i><?php echo $room['rating']?>
                     </div>

712cf55f00985284633f53c7c9152a047c5ba3cb
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-01-03 17:53:30 +0700

    feat/favorite

--- app/Models/booking_history/booking_history.model.php
+++ app/Models/booking_history/booking_history.model.php
@@ -8,8 +8,7 @@ function get_user_booking_info($user_id)
     INNER JOIN booking ON users.id = booking.id_user
     INNER JOIN bill ON bill.id_booking = booking.id
     INNER JOIN rooms ON booking.id_room = rooms.id
-    WHERE users.id = :user_id;
-    -- GROUP BY rooms.id";
+    WHERE users.id = :user_id;";
     $statement = $connection->prepare($query);
     $statement->bindParam(':user_id', $user_id);
     $statement->execute();  

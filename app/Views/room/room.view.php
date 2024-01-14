1e33a9e568a38a54ee39dd84073cdc00572042b1
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-01-14 12:42:07 +0700

    refactor

--- app/Views/room/room.view.php
+++ app/Views/room/room.view.php
@@ -1,6 +1,8 @@
 <?php 
     require ("app/Views/layouts/header.php") ;
     require ("app/Views/layouts/navbar.php");
+    require ("app/Views/auth/login.php");
+    require ("app/Views/auth/register.view.php");
     require ("app/Views/room/banner.room.view.php");
     require ("app/Views/home/content.php");
     require ("app/Views/home/card.php");

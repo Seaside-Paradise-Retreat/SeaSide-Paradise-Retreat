09d9608aef68953ad8d1b502b992993684b01ec1
Admin <admin@vinasupport.com>
2024-01-20 08:42:16 +0700

    Fix error

--- app/Databases/database.php
+++ app/Databases/database.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $database = "Seaside_paradise_retreat";
 $username = "root";
-$password = "";
+$password = "mysql";
 
 $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4"; //$dsn: Database source name
 try {
